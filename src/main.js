5b46e503baf5c56d3a06ecab0f412df9f5d010c9
Kamesuta <kamesuta@gmail.com>
2020-10-28 03:25:31 +0900

    Update discord.js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,38 @@
-const Discord = require('discord.js');
-const config = require('config');
-const i18n = require("i18n");
+const Discord = require('discord.js')
+const config = require('config')
+const i18n = require("i18n")
 const {format} = require('util')
+const template = require('string-placeholder')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 i18n.configure({
   locales: ['en', 'ja'],
   defaultLocale: config.locale,
   directory: __dirname + '/../locales',
-});
+})
 
 client.on('ready', () => {
-  console.log(i18n.__('log.login', { user: client.user.tag }));
-});
+  console.log(i18n.__('log.login', {user: client.user.tag}))
+})
 
 client.on('guildMemberRemove', async member => {
   await Promise.all(config.channels.map(channelData => {
     return (async () => {
       if (member.guild.id !== channelData.guild)
-        return;
-      const channel = client.channels.resolve(channelData.channel);
-      channel.send(format(channelData.message, member.guild.name, member.displayName, member.user.tag, member.user.id));
-    })();
-  }));
-});
+        return
+      const channel = client.channels.resolve(channelData.channel)
+      channel.send(template(
+        channelData.message,
+        {
+          guild: member.guild.name,
+          nickname: member.displayName,
+          name: member.user.tag,
+          id: member.user.id
+        }
+      ))
+    })()
+  }))
+})
 
-client.login(config.token);
+client.login(config.token)
